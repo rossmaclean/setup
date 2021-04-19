f5e07c043b7a01ba7d4915330ca6abb9dab8cf36
Ross MacLean <ross@rossmac.co.uk>
2021-04-19 19:35:47 +0100

    Update laptop.sh

--- laptop.sh
+++ laptop.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "Running setup for laptop"
 
+echo "Change Pacman to total download"
+sudo sed -i -e 's/#TotalDownload/TotalDownload/g' /etc/pacman.conf
+
 echo "Updating system"
 sudo pacman -Syyu
 
@@ -13,8 +16,11 @@ cd yay
 makepkg -si
 
 echo "Installing packages using Pacman"
-sudo pacman -S discord chromium nodejs jdk11-openjdk jdk8-openjdk npm virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat docker docker-compose
+sudo pacman -S discord chromium nodejs jdk11-openjdk jdk8-openjdk npm virt-manager qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat docker docker-compose lutris
 sudo usermod -aG docker ross
+
+echo "Enabling dhcpcd service"
+sudo systemctl enable dhcpcd.service --now
 
 echo "Installing packages using Yay"
 yay -S jetbrains-toolbox
@@ -23,7 +29,6 @@ yay -S icaclient
 yay -S spotify
 
 echo "Enabling libvirtd service"
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
+sudo systemctl enable libvirtd.service --now
 
 echo "All done"
